457b625efb42fdd5a88ef4a9b057e01c17cf702f
randomdev1234 <randomdev1234@protonmail.com>
2021-08-05 15:04:13 -0600

    WIP

--- src/webapi/entry.cgi.js
+++ src/webapi/entry.cgi.js
@@ -7,15 +7,18 @@ const entryCGI4 = fs.readFileSync(path.join(__dirname, 'entry.cgi.4.js')).toStri
 const entryCGI5 = JSON.stringify(require('./entry.cgi.5.json'))
 
 module.exports = (library, req, res) => {
-  if (req.queryData.api === 'SYNO.Core.Desktop.Defs') {
+  if (req.queryData.method === 'getjs') {
+    res.setHeader('content-type', 'application/json; charset="UTF-8"')
+  }
+  if (req.queryData.api === 'SYNO.Core.Desktop.Defs' && req.queryData.version === '1' && req.queryData.method === 'getjs') {
     return res.end(entryCGI1)
-  } else if (req.queryData.api === 'SYNO.Core.Desktop.JSUIString') {
+  } else if (req.queryData.api === 'SYNO.Core.Desktop.JSUIString' && req.queryData.version === '1' && req.queryData.method === 'getjs') {
     return res.end(entryCGI2)
-  } else if (req.queryData.api === 'SYNO.Core.Desktop.SessionData') {
-    return res.end(entryCGI3.replace('SESSIONID', `${process.env.SYNOLOGY_SESSION_ID || 'p0KZoueC'}.${process.env.SYNOLOGY_SERIAL_NUMBER || 'MEqA1130LWN011720'}`))
-  } else if (req.queryData.api === 'SYNO.Core.Desktop.UIString') {
+  } else if (req.queryData.api === 'SYNO.Core.Desktop.SessionData' && req.queryData.version === '1' && req.queryData.method === 'getjs') {
+    return res.end(entryCGI3)
+  } else if (req.queryData.api === 'SYNO.Core.Desktop.UIString' && req.queryData.version === '1' && req.queryData.method === 'getjs') {
     return res.end(entryCGI4)
-  } else if (req.postData.api === 'SYNO.Core.Desktop.Initdata' && req.postData.method === 'get') {
+  } else if (req.postData.api === 'SYNO.Core.Desktop.Initdata' && req.postData.launch_app === '"SYNO.SDS.AudioStation.Application"') {
     return res.end(entryCGI5)
   } else if (req.postData.api === 'SYNO.Core.Desktop.Initdata' && req.postData.action === '"external_ip"') {
     return res.end('{ "data": { "external_ip": "0.0.0.0" }, "success": true }')
@@ -31,10 +34,10 @@ module.exports = (library, req, res) => {
     return res.end('{ "data": { "has_fail": false, "result": [{ "api": "SYNO.Core.Desktop.Timeout", "method": "check", "success": true, "version": 1 }] }, "success": true }')
   }
   // AudioStation pinning and playlists routes through this URL
-  if (req.postData.api === 'SYNO.AudioStation.Pin') {
+  if (req.req.postData.api === 'SYNO.AudioStation.Pin') {
     const pinList = require('../webapi/AudioStation/pinlist.js')
     return pinList(library, req, res)
-  } else if (req.postData.api === 'SYNO.AudioStation.Browse.Playlist') {
+  } else if (req.req.postData.api === 'SYNO.AudioStation.Browse.Playlist') {
     const playList = require('../webapi/AudioStation/playlist.cgi.js')
     return playList(library, req, res)
   }
