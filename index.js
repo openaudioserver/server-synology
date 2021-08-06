fd9dc70523042b452a294af041aaa7a8240e2065
randomdev1234 <randomdev1234@protonmail.com>
2021-08-06 08:09:46 -0600

    WIP

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const util = require('util')
 const existsCache = {}
 const bufferCache = {}
 const modulePath = __dirname
+const SMID = 'E9GRXinHCOWZrGINa_VJPiuzMjMWLwkXyJCJT8z_pZ7KZ8Jf7vdLq9vxSf4zpJ7lQAWaO6WOHOZYkVfAnziPPg'
+const ID = 'p0KZoueC'
+const SERIAL = 'MEqA1130LWN011720'
+const SESSION = '38076bb1fbef4df0cc4523b93124d4d6aca326cf23c421013aa0552f554d4eb44490'
 
 const existsAsync = util.promisify((filePath, callback) => {
   try {
@@ -37,9 +41,6 @@ module.exports = {
   },
   handleRequest: async (library, req, res) => {
     res.statusCode = 200
-    if (!req.headers.cookie || req.headers.cookie.indexOf('smid=') === -1) {
-      res.setHeader('set-cookie', `_session=${process.env.SYNOLOGY_SESSION || '10be75671cbb80fa4b45bd0501514a7bd664f6d669294d2199b916dd1ecceb8f4531'}; smid=${process.env.SYNOLOGY_SMID || 'E9GRXinHCOWZrGINa_VJPiuzMjMWLwkXyJCJT8z_pZ7KZ8Jf7vdLq9vxSf4zpJ7lQAWaO6WOHOZYkVfAnziPPg'}; stay_login=1; id=${process.env.SYNOLOGY_SESSION_ID || 'p0KZoueC'}.${process.env.SYNOLOGY_SERIAL_NUMBER || 'MEqA1130LWN011720'}`)
-    }
     if (req.urlPath.indexOf('.cgi/') > -1) {
       req.urlPath = req.urlPath.substring(0, req.urlPath.indexOf('.cgi/') + '.cgi'.length)
     }
@@ -65,7 +66,8 @@ module.exports = {
       }
     }
     if (req.homePath) {
-      return serveStaticFile(req, res, process.env.DSAUDIO_HTML_PATH)
+      res.setHeader('set-cookie', `smid=${SMID}; id=${ID}.${SERIAL}; _session=${SESSION}; samesite=none; secure;`)
+      return serveHomePage(req, res)
     }
     if (req.synomanPath) {
       return serveStaticFile(req, res, req.synomanPath)
@@ -100,4 +102,96 @@ async function executeRoute (library, req, res, filePath) {
     res.statusCode = 500
     return res.end()
   }
+}
+
+async function serveHomePage (req, res) {
+  const css = [
+    '/scripts/ext-3/resources/css/ext-all.css',
+    '/scripts/ext-3/resources/css/xtheme-gray.css',
+    '/scripts/ext-3/ux/ux-all.css',
+    '/synoSDSjslib/sds.css',
+    '/webman/resources/css/desktop.css',
+    '/webman/modules/AHAManager/style.css',
+    '/webman/modules/EzInternet/style.css',
+    '/webman/modules/Share/style.css',
+    '/webman/modules/SecurityScan/style.css',
+    '/webman/modules/Utils/style.css',
+    '/webman/modules/StorageManager/style.css',
+    '/webman/modules/WelcomeApp/style.css',
+    '/webman/modules/DiskMessageHandler/style.css',
+    '/webman/modules/AudioPlayer/style.css',
+    '/webman/modules/ThumbConvertProgress/style.css',
+    '/webman/modules/FileBrowser/style.css',
+    '/webman/modules/MyDSCenter/style.css',
+    '/webman/modules/BandwidthControl/style.css',
+    '/webman/modules/SystemInfoApp/style.css',
+    '/webman/modules/iSCSI/style.css',
+    '/webman/modules/SupportForm/style.css',
+    '/webman/modules/Widgets/style.css',
+    '/webman/modules/WelcomeTip/style.css',
+    '/webman/modules/PkgManApp/style.css',
+    '/webman/modules/HelpBrowser/style.css',
+    '/webman/modules/ConfigBackup/style.css',
+    '/webman/modules/ResourceMonitor/style.css',
+    '/webman/modules/LogCenter/style.css',
+    '/webman/modules/DataDrivenDocuments/style.css',
+    '/webman/modules/TaskSchedulerUtils/style.css',
+    '/webman/modules/DSMNotify/style.css',
+    '/webman/modules/PollingTask/style.css',
+    '/webman/modules/HotkeyManager/style.css',
+    '/webman/modules/VideoPlayer2/style.css',
+    '/webman/modules/ClipBoardJS/style.css',
+    '/webman/modules/FileTaskMonitor/style.css',
+    '/webman/modules/AviaryEditor/style.css',
+    '/webman/modules/AdminCenter/style.css',
+    '/webman/modules/PersonalSettings/style.css',
+    '/webman/modules/ExternalDevices/style.css',
+    '/webman/modules/PhotoViewer/style.css',
+    '/webman/modules/C3/style.css',
+    '/webman/modules/TaskSchedulerWidget/style.css',
+    '/webman/3rdparty/OAuthService/style.css',
+    '/webman/3rdparty/AudioStation/style.css',
+    '/webman/3rdparty/SynoFinder/style.css'
+  ]
+  const js = [
+    '/webapi/entry.cgi?api=SYNO.Core.Desktop.Defs&version=1&method=getjs',
+    '/webapi/entry.cgi?api=SYNO.Core.Desktop.JSUIString&version=1&method=getjs&lang=enu',
+    '/webapi/entry.cgi?api=SYNO.Core.Desktop.UIString&version=1&method=getjs&lang=enu',
+    '/scripts/prototype-1.7.2/prototype.js',
+    '/scripts/ext-3/adapter/ext/ext-base.js',
+    '/scripts/ext-3/ext-all.js',
+    '/scripts/ext-3/ux/ux-all.js',
+    '/scripts/scrollbar/flexcroll.js',
+    '/synoSDSjslib/sds.js',
+    '/webman/desktop.js',
+    '/webapi/entry.cgi?api=SYNO.Core.Desktop.SessionData&version=1&method=getjs',
+    '/webman/security.cgi'
+  ]
+  const tags = []
+  for (const file of css) {
+    tags.push(`<link rel="stylesheet" href="${file}" />`)
+  }
+  for (const file of js) {
+    tags.push(`<script src="${file}" />`)
+  }
+  return res.end(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Synology compatibility for Open Audio Server</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=11" />
+    <meta name="msapplication-TileImage" content="resources/images/icon_tile.png" />
+    <meta name="application-name" content="AudioStation" />
+    <meta name="msapplication-TileColor" content="#246BB3" />
+    <link rel="apple-touch-icon" href="webman/resources/images/icon_dsm_96.png" />
+    <link rel="mask-icon"  color="#0086E5" href="webman/safari_pin_icon.svg" />
+    <link rel="shortcut icon" href="webman/favicon.ico" />
+    <link rel="shortcut icon" sizes="96x96" href="webman/resources/images/icon_dsm_96.png" />
+    <link rel="shortcut icon" sizes="64x64" href="webman/resources/images/icon_dsm_64.png" />
+    <link rel="shortcut icon" sizes="48x48" href="webman/resources/images/icon_dsm_48.png" />
+    <link rel="shortcut icon" sizes="32x32" href="webman/resources/images/icon_dsm_32.png" />
+    <link rel="shortcut icon" sizes="16x16" href="webman/resources/images/icon_dsm_16.png" />
+    ${tags.join('\n')}
+  </head>
+  <body role="application"></body></html>`)
 }
--- src/webapi/AudioStation/genre.cgi.js
+++ src/webapi/AudioStation/genre.cgi.js
@@ -20,31 +20,12 @@ module.exports = async (library, req, res) => {
 }
 
 async function listGenres (library, options) {
-  const offset = options.offset ? parseInt(options.offset, 10) || 0 : 0
-  const limit = options.limit ? parseInt(options.limit, 10) || 0 : 0
-  const response = {
-    data: {
-      genres: library.genres,
-      offset: offset || 0
-    },
-    success: true
-  }
-  if (options.keyword) {
-    response.data.genres = response.data.genres.filter(genre => genre.name && genre.name.toLowerCase().indexOf(options.keyword.toLowerCase()) > -1)
-  }
-  if (!options.sort_by || options.sort_by === 'name') {
-    response.data.genres = response.data.genres.sort((a, b) => {
-      if (!options.sort_direction || options.sort_direction === 'ASC') {
-        return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1
-      } else {
-        return a.name.toLowerCase() < b.name.toLowerCase() ? 1 : -1
-      }
-    })
-  }
-  response.data.total = response.data.genres.length
-  if (limit && response.data.genres.length > limit) {
-    response.data.genres.length = limit
-  }
+  const response = await library.getObjects(library.genres, {
+    sortBy: options.sort_by,
+    sortDirection: options.sort_direction,
+    offset: options.offset,
+    limit: options.limit
+  })
   return response
 }
 
@@ -55,11 +36,13 @@ async function listDefaultGenres (library) {
     },
     success: true
   }
-  for (const genre of library.genres) {
-    const imagePath = path.join(process.env.SYNOMAN_PATH, `/webman/3rdparty/AudioStation/images/_2x/cover_${genre.name.toLowerCase()}.png`)
-    const exists = existsCache[imagePath] = existsCache[imagePath] || fs.existsSync(imagePath)
-    if (exists) {
-      response.data.default_genres.push(genre)
+  if (library.genres) {
+    for (const genre of library.genres) {
+      const imagePath = path.join(process.env.SYNOMAN_PATH, `/webman/3rdparty/AudioStation/images/_2x/cover_${genre.name.toLowerCase()}.png`)
+      const exists = existsCache[imagePath] = existsCache[imagePath] || fs.existsSync(imagePath)
+      if (exists) {
+        response.data.default_genres.push(genre)
+      }
     }
   }
   return response
