9e5ad3613495cda70ccfb41a46415ae6b1491747
randomdev1234 <randomdev1234@protonmail.com>
2021-08-06 10:50:54 -0600

    WIP

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ module.exports = {
     const homePath = req.urlPath === '/dsaudio'
     if (homePath) {
       req.homePath = true
+      req.staticPath = path.join(modulePath, 'src', 'dsaudio.html')
       return true
     }
     const sourcePath = path.join(modulePath, 'src', `${req.urlPath}.js`)
@@ -80,7 +81,7 @@ module.exports = {
       }
     }
     if (req.homePath) {
-      return serveHomePage(req, res)
+      return serveStaticFile(req, res)
     }
     if (req.staticPath) {
       return serveStaticFile(req, res, req.staticPath)
@@ -115,96 +116,4 @@ async function executeRoute (library, req, res, filePath) {
     res.statusCode = 500
     return res.end()
   }
-}
-
-async function serveHomePage (req, res) {
-  const css = [
-    '/scripts/ext-3/resources/css/ext-all.css',
-    '/scripts/ext-3/resources/css/xtheme-gray.css',
-    '/scripts/ext-3/ux/ux-all.css',
-    '/synoSDSjslib/sds.css',
-    '/webman/resources/css/desktop.css',
-    '/webman/modules/AHAManager/style.css',
-    '/webman/modules/EzInternet/style.css',
-    '/webman/modules/Share/style.css',
-    '/webman/modules/SecurityScan/style.css',
-    '/webman/modules/Utils/style.css',
-    '/webman/modules/StorageManager/style.css',
-    '/webman/modules/WelcomeApp/style.css',
-    '/webman/modules/DiskMessageHandler/style.css',
-    '/webman/modules/AudioPlayer/style.css',
-    '/webman/modules/ThumbConvertProgress/style.css',
-    '/webman/modules/FileBrowser/style.css',
-    '/webman/modules/MyDSCenter/style.css',
-    '/webman/modules/BandwidthControl/style.css',
-    '/webman/modules/SystemInfoApp/style.css',
-    '/webman/modules/iSCSI/style.css',
-    '/webman/modules/SupportForm/style.css',
-    '/webman/modules/Widgets/style.css',
-    '/webman/modules/WelcomeTip/style.css',
-    '/webman/modules/PkgManApp/style.css',
-    '/webman/modules/HelpBrowser/style.css',
-    '/webman/modules/ConfigBackup/style.css',
-    '/webman/modules/ResourceMonitor/style.css',
-    '/webman/modules/LogCenter/style.css',
-    '/webman/modules/DataDrivenDocuments/style.css',
-    '/webman/modules/TaskSchedulerUtils/style.css',
-    '/webman/modules/DSMNotify/style.css',
-    '/webman/modules/PollingTask/style.css',
-    '/webman/modules/HotkeyManager/style.css',
-    '/webman/modules/VideoPlayer2/style.css',
-    '/webman/modules/ClipBoardJS/style.css',
-    '/webman/modules/FileTaskMonitor/style.css',
-    '/webman/modules/AviaryEditor/style.css',
-    '/webman/modules/AdminCenter/style.css',
-    '/webman/modules/PersonalSettings/style.css',
-    '/webman/modules/ExternalDevices/style.css',
-    '/webman/modules/PhotoViewer/style.css',
-    '/webman/modules/C3/style.css',
-    '/webman/modules/TaskSchedulerWidget/style.css',
-    '/webman/3rdparty/OAuthService/style.css',
-    '/webman/3rdparty/AudioStation/style.css',
-    '/webman/3rdparty/SynoFinder/style.css'
-  ]
-  const js = [
-    '/webapi/entry.cgi?api=SYNO.Core.Desktop.Defs&version=1&method=getjs',
-    '/webapi/entry.cgi?api=SYNO.Core.Desktop.JSUIString&version=1&method=getjs&lang=enu',
-    '/webapi/entry.cgi?api=SYNO.Core.Desktop.UIString&version=1&method=getjs&lang=enu',
-    '/scripts/prototype-1.7.2/prototype.js',
-    '/scripts/ext-3/adapter/ext/ext-base.js',
-    '/scripts/ext-3/ext-all.js',
-    '/scripts/ext-3/ux/ux-all.js',
-    '/scripts/scrollbar/flexcroll.js',
-    '/synoSDSjslib/sds.js',
-    '/webman/desktop.js',
-    '/webapi/entry.cgi?api=SYNO.Core.Desktop.SessionData&version=1&method=getjs',
-    '/webman/security.cgi'
-  ]
-  const cssTags = []
-  for (const file of css) {
-    cssTags.push(`<link rel="stylesheet" href="${file}" />`)
-  }
-  const jsTags = []
-  for (const file of js) {
-    jsTags.push(`<script src="${file}"></script>`)
-  }
-  res.setHeader('content-type', 'text/html')
-  return res.end(`<!DOCTYPE html>
-<html>
-  <head>
-    <title>Open Audio Server:  Synology</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=11" />
-    <meta name="application-name" content="AudioStation" />
-    <meta name="msapplication-TileImage" content="/favicon-96x96.png" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="shortcut icon" sizes="96x96" href="/favicon-96x96.png" />
-    <link rel="shortcut icon" sizes="64x64" href="/favicon-64x64.png" />
-    <link rel="shortcut icon" sizes="48x48" href="/favicon-48x48.png" />
-    <link rel="shortcut icon" sizes="32x32" href="/favicon-32x32.png" />
-    <link rel="shortcut icon" sizes="16x16" href="/favicon-16x16.png" />
-    ${cssTags.join('\n')}
-  </head>
-  <body role="application"></body>${jsTags.join('\n')}</html>`)
 }
