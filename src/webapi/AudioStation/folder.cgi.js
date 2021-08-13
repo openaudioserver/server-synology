0d8b00f489eab1171acd82d1b95d8ae56fb6728f
randomdev1234 <randomdev1234@protonmail.com>
2021-08-12 19:41:00 -0600

    WIP

--- src/webapi/AudioStation/folder.cgi.js
+++ src/webapi/AudioStation/folder.cgi.js
@@ -13,70 +13,39 @@ module.exports = async (library, req, res) => {
 }
 
 async function listContents (library, options) {
-  const offset = options.offset ? parseInt(options.offset, 10) || 0 : 0
-  const limit = options.limit ? parseInt(options.limit, 10) || 0 : 0
   const response = {
     data: {
-      offset: offset || 0
+      items: [],
+      offset: options.offset || 0,
+      limit: options.limit || 1000,
+      total: options.total || 0
     },
     success: true
   }
   if (options.id) {
-    const folder = Object.values(library.folders).filter(folder => folder.id === options.id)[0]
-    response.data.items = folder.items
-    response.data.id = folder.id
-  } else {
-    if (!library.folders[process.env.MUSIC_PATH]) {
-      response.data.items = []
-      response.data.id = 'dir_1'
-    } else {
-      response.data.items = library.folders[process.env.MUSIC_PATH].items
-      response.data.id = library.folders[process.env.MUSIC_PATH].id
-    }
-  }
-  if (options.sort_by) {
-    for (const integerField of ['duration', 'disc', 'year', 'track']) {
-      if (options.sort_by === integerField) {
-        response.data.items = response.data.items.sort((a, b) => {
-          if (options.sort_direction === 'ASC') {
-            if (a.additional.song_audio[integerField]) {
-              return a.additional.song_audio[integerField] > b.additional.song_audio[integerField] ? 1 : -1
-            } else {
-              return a.additional.song_tag[integerField] > b.additional.song_tag[integerField] ? 1 : -1
-            }
-          } else {
-            if (a.additional.song_audio[integerField]) {
-              return a.additional.song_audio[integerField] < b.additional.song_audio[integerField] ? 1 : -1
-            } else {
-              return a.additional.song_tag[integerField] < b.additional.song_tag[integerField] ? 1 : -1
-            }
-          }
-        })
-      }
-      if (options.sort_by === 'name' || options.sort_by === 'title') {
-        response.data.items = response.data.items.sort((a, b) => {
-          return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
-        })
-      }
+    const item = await library.getTreeItem(options.id)
+    for (const child of item.contents) {
+      response.data.items.push({
+        id: child.id,
+        is_personal: false,
+        path: child.path,
+        title: child.path,
+        type: child.type
+      })
     }
   } else {
-    response.data.items = response.data.items.sort((a, b) => {
-      return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1
-    })
+    for (const item of library.tree.contents) {
+      response.data.items.push({
+        id: item.id,
+        is_personal: false,
+        path: item.path,
+        title: item.path,
+        type: item.type
+      })
+    }
+    response.data.items = library.getObjects(response.data.items, options)
   }
   response.data.total = response.data.items.length
-  response.data.folder_total = response.data.items.filter(folder => folder.type === 'folder').length
-  if (limit && response.data.items.length > limit) {
-    response.data.items.length = limit
-  }
-  response.data.items = JSON.parse(JSON.stringify(response.data.items))
-  const fields = ['genres', 'items', 'composers', 'artists', 'hasImage', 'albumFolder', 'artistFolder', 'songFile', 'artist', 'album']
-  for (const item of response.data.items) {
-    for (const field of fields) {
-      delete (item[field])
-    }
-  }
-  const folders = response.data.items.filter(item => item.type === 'folder')
-  response.data.folder_total = folders && folders.length ? folders.length : 0
+  response.data.folder_total = response.data.items.filter(item => item.type === 'folder').length
   return response
 }
